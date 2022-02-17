4df1a58a92a0c0ca8280a589f1760695615b7006
Tr44shy <95266761+Tr44shy@users.noreply.github.com>
2022-02-17 03:57:08 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        public string Name;
+
+        private static Class1 instance = null;
+
+        public String Value;
+
+        protected Class1() {
+           
+        }
+
+        public static Class1 Instance()
+        {
+           
+                if (instance == null) {
+                    instance = new Class1();
+                }
+                return instance;
+           
+        }
+
+        public void SetValue(String valor){
+
+            Value = valor;
+
+        }
+
+        public String GetValue() {
+            return "Hola " + Value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = Class1.Instance();
+            Class1 obj2 = Class1.Instance();
+
+            obj1.SetValue("Pedro");
+            //el valor seteado en el obj1 es invocable desde el obj2 porque comparten una misma instancia
+            Console.WriteLine(obj2.GetValue());
+
+        }
+    }
+}
